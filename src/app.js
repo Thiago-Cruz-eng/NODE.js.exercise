eb43f939072d87a6b11529f962427167a663f726
Thiago Cruz <morim24093@gmail.com>
2022-03-26 09:39:59 -0300

    finish the project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,67 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get("/repositories", (req, res) => { 
+  return res.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", (req, res) => {
+  const {tittle, url, techs} = req.body;
+  const repository = {id: uuid(), tittle, url, techs, likes: 0};
+ 
+  repositories.push(repository);
+ 
+  return res.json(repository)
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", (req, res) => {
+  const {id} = req.params
+  const {tittle, url, techs} = req.body;
+ 
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+ 
+  if(repositoryIndex < 0) {
+   return res.status(400).json({message:'Usuario nao encontrado!'})
+  }
+ 
+  const repository = {
+   id,
+   tittle,
+   url,
+   techs,
+   likes: repositories[repositoryIndex].likes
+  };
+ 
+  repositories[repositoryIndex] = repository;
+ 
+  return res.json(repository)
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", (req, res) => {
+  const {id} = req.params 
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+ 
+  if(repositoryIndex < 0) {
+   return res.status(400).json({message:'Usuario nao encontrado!'})
+  }
+ 
+  repositories.splice(repositoryIndex, 1);
+ 
+  return res.status(204).send()
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", (req, res) => {
+  const {id} = req.params
+  const {tittle, url, techs} = req.body;
+ 
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+ 
+  if(repositoryIndex < 0) {
+   return res.status(400).json({message:'Usuario nao encontrado!'})
+  }
+
+  repositories[repositoryIndex].likes++;
+  return res.json(repositories[repositoryIndex])
 });
 
 module.exports = app;
